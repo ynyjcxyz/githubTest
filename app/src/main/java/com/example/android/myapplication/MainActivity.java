b416a5b3e3596f446bb8b56271bbbfff69b026b3
John Young <ynyjcxyz@gmail.com>
2021-07-28 04:52:52 -0700

    first test

---
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.android.myapplication.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printToLogs(View view) {
+        // Find first menu item TextView and print the text to the logs
+
+        // Find second menu item TextView and print the text to the logs
+
+        // Find third menu item TextView and print the text to the logs
+
+    }
+}
