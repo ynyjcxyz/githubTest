939371de2dce9f7247f87908cd16b7741dcef6ff
John Young <ynyjcxyz@gmail.com>
2021-07-28 18:40:45 -0700

    get method from textView and print to log

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.android.myapplication.R;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void printToLogs(View view) {
+        //Log.i("Here is a message", "We come! We fight! We win!");
+        TextView textObject1 = (TextView) findViewById(R.id.menu_item_1);
+        String text1 = (String) textObject1.getText();
+        Log.i("First menu item Text", text1);
+
+        TextView textObject2 = (TextView) findViewById(R.id.menu_item_2);
+        String text2 = (String) textObject2.getText();
+        Log.i("Second menu item Text", text2);
+
+        TextView textObject3 = (TextView) findViewById(R.id.menu_item_3);
+        String text3 = (String) textObject3.getText();
+        Log.i("Third menu item Text", text3);
+
+
         // Find first menu item TextView and print the text to the logs
 
         // Find second menu item TextView and print the text to the logs
